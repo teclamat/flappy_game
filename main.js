100f01d4e308a9379c3559d6ed0b4e24d7ea8008
PawCyb9809 <19918.ppl@gmail.com>
2019-05-30 15:37:46 +0200

    [FLAPPY-12] Added Input Handling (console log space)

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-class FlappyBirdGame {
-    build(){
-    const cvs = document.querySelector('#Flappy');
-    const ctx = cvs.getContext("2d");
-    }
-    render(){
-        
+class inputHandling{
+    constructor(ctx){
+        window.addEventListener("keydown", function(event) {
+            if(event.code=="Space"){
+                console.log(event.code);
+            }else{
+                return;
+            }
+          }, true)
     }
 }
+
+class FlappyBirdGame {
+    constructor() {
+        const canv = document.createElement("canvas");
+        document.body.appendChild(canv);
+        canv.id = 'Flappy';
+        canv.width = 300;
+        canv.height = 530;
+        const ctx = canv.getContext('2d');
+        new inputHandling(ctx);
+    }
+    build(){
+
+    }
+    render(){
+
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const flappygame = new FlappyBirdGame();
+});
