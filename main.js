3987412c058b27789b3fcb592fff9d3016ab450b
PawCyb9809 <19918.ppl@gmail.com>
2019-06-04 14:49:16 +0200

    [FLAPPY-12] Introduced changes

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
-class inputHandling {
-    constructor(ctx) {
-        window.addEventListener("keydown", function(event) {
-            if(event.code=="Space") {
-                console.log(event.code);
-            } else {
-                return;
-            }
-          }, true)
-    }
-}
-
 class FlappyBirdGame {
     constructor() {
-        const canv = document.createElement("canvas");
-        document.body.appendChild(canv);
-        canv.id = 'Flappy';
-        canv.width = 300;
-        canv.height = 530;
-        const ctx = canv.getContext('2d');
-        new inputHandling(ctx);
+        this.canv = document.createElement("canvas");
+        document.body.appendChild(this.canv);
+        this.canv.width = 440;
+        this.canv.height = 600;
+        this.ctx = this.canv.getContext('2d');
+        window.addEventListener('keydown', (ev) => this.handleInput(ev));
     }
-    build(){
+
+    handleInput(event) {
+        if (event.code == "Space") {
+            console.log(event.code);
+        }
+    }
+
+    build() {
 
     }
-    render(){
+    render() {
 
     }
 }
